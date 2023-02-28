c6615634120bd3103793dbe27e84caa0fe26d4f5
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2023-02-28 10:54:20 +0200

    add assert in tests

--- Tests/test_authorization_right_credentials.py
+++ Tests/test_authorization_right_credentials.py
@@ -7,4 +7,4 @@ def test_well_authorization(username, password, login_page, dashboard_page):
     login_page.input_username(username)
     login_page.input_password(password)
     login_page.click_login_button()
-    dashboard_page.check_the_head_is_displayed()
+    assert dashboard_page.check_the_head_is_displayed()
